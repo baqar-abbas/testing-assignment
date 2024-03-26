1a24c5a21379e822ed067ce3ef41ee6897b2e09f
baqar-abbas <baqar007@hotmail.com>
2024-03-26 11:20:54 +0500

    Implement a filtering feature in categories component.

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,26 +1,59 @@
-/* eslint-disable */
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'; // To access the state and dispatch actions
-import { checkStatus } from '../redux/categories/categoriesSlice'; // Import the checkStatus action creator
+import { fetchBooks } from '../redux/books/booksApi'; // Import the fetchBooks async thunk
 
+// Implement the functionality of filtering books by category
 const Categories = () => {
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [filteredBooks, setFilteredBooks] = useState([]);
+  const [hasCheckedStatus, setHasCheckedStatus] = useState(false);
+  const { books } = useSelector((state) => state.allbooks);
   const categories = useSelector((state) => state.categories.categories);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
+  const handleCheckStatus = () => {
+    const filtered = books.filter((book) => book.category === selectedCategory);
+    setFilteredBooks(filtered);
+    setHasCheckedStatus(true);
+  };
+
   return (
     <>
+      <select onChange={(e) => setSelectedCategory(e.target.value)}>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
       <button
         type="button"
         className="btn btn-status"
         style={{
           marginTop: '5rem', backgroundColor: '#009FBD', fontSize: '16px', height: '3em', width: '10rem',
         }}
-        onClick={() => dispatch(checkStatus())}
+        onClick={handleCheckStatus}
       >
         Check Status
       </button>
-      <ul>
-        {categories.map((category, index) => <li key={index}>{category}</li>)}
-      </ul>
+
+      {selectedCategory && hasCheckedStatus && filteredBooks.length === 0 ? (
+        <div>
+          <p>No books available for the selected category</p>
+        </div>
+      ) : (
+        filteredBooks.map((book) => (
+          <div key={book.item_id}>
+            <h3>{book.title}</h3>
+            <p>{book.author}</p>
+            <p>{book.category}</p>
+          </div>
+        ))
+      )}
     </>
   );
 };
